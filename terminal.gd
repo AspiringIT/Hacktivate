e5557eecefc258402f661925166609cd0f9c6bab
AspiringIT <nicktab20@gmail.com>
2024-03-08 18:47:26 -0600

    Created Terminal Base. There is a lnown error in the Terminal script as the scrollbar changed is being called

---
+++ terminal.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+const InputResponse = preload("res://InputResponse.tscn")
+
+@onready var history_rows = $Backround/MarginContainer/Rows/GameInfo/Scroll/HistoryRows
+@onready var scroll = $Backround/MarginContainer/Rows/GameInfo/Scroll
+@onready var scrollbar = scroll.get_v_scroll_bar()
+
+func _ready() -> void:
+	scrollbar.connect("changed", self,"handle_scrollbar_changed")
+
+
+func handle_scrollbar_changed():
+	scroll.scroll_vertical = scrollbar.max_value
+
+func _on_input_text_submitted(new_text: String) -> void:
+	var input_response = InputResponse.instantiate()
+	input_response.set_text(new_text, "This is where a response will go")
+	history_rows.add_child(input_response)
