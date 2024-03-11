0c4c45e05330756ec43d23f88a112b0328b8685c
AspiringIT <nicktab20@gmail.com>
2024-03-11 16:13:54 -0500

    Added the start of a curl command

--- Command Procesor.gd
+++ Command Procesor.gd
@@ -5,7 +5,7 @@ func process_command(input: String) -> String:
 	if words.size() == 0:
 		return "Error: No words were parsed"
 	var first_word = words[0].to_lower()
-	var second_word =""
+	var second_word = ""
 	if words.size() > 1:
 		second_word = words[1].to_lower()
 	match first_word:
@@ -13,15 +13,20 @@ func process_command(input: String) -> String:
 			return go(second_word)
 		"help":
 			return help()
-		_: #If there are no other cases this executes
-			return "Unreconized command - please try again"
-			
-			
-			
+		"curl":
+			return curl(second_word)
+		_:
+			return "Unrecognized command - please try again"
+
 func go(second_word: String) -> String:
 	if second_word == "":
-		return "Not enough paramaters, please check your command and try again"
-	return "You go %s" % second_word
-	
+		return "Not enough parameters, please check your command and try again"
+	return "You go " + second_word
+
+func curl(second_word: String) -> String:
+	if second_word == "example.com":
+		return '<!doctype html>\n<html>\n<head>\n    <title>Example Domain</title>\n\n    <meta charset="utf-8" />\n    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1" />\n    <style type="text/css">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 2em;\n        background-color: #fdfdff;\n        border-radius: 0.5em;\n        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        div {\n            margin: 0 auto;\n            width: auto;\n        }\n    }\n    </style>    \n</head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission.</p>\n    <p><a href="https://www.iana.org/domains/example">More information...</a></p>\n</div>\n</body>\n</html>'
+	return "error"
+
 func help() -> String:
 	return "You can use these commands go [location], help"
