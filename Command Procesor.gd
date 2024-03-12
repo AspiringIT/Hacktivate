7293cf835cb504dac33820ca43e44de64940a615
AspiringIT <nicktab20@gmail.com>
2024-03-12 17:11:55 -0500

    Having issues adding 4th word to curl func

--- Command Procesor.gd
+++ Command Procesor.gd
@@ -44,8 +44,12 @@ func curl(second_word: String, third_word: String) -> String:
 	if second_word == "example.com":
 		return '<!doctype html>\n<html>\n<head>\n    <title>Example Domain</title>\n\n    <meta charset="utf-8" />\n    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />\n    <meta name="viewport" content="width=device-width, initial-scale=1" />\n    <style type="text/css">\n    body {\n        background-color: #f0f0f2;\n        margin: 0;\n        padding: 0;\n        font-family: -apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;\n        \n    }\n    div {\n        width: 600px;\n        margin: 5em auto;\n        padding: 2em;\n        background-color: #fdfdff;\n        border-radius: 0.5em;\n        box-shadow: 2px 3px 7px 2px rgba(0,0,0,0.02);\n    }\n    a:link, a:visited {\n        color: #38488f;\n        text-decoration: none;\n    }\n    @media (max-width: 700px) {\n        div {\n            margin: 0 auto;\n            width: auto;\n        }\n    }\n    </style>    \n</head>\n\n<body>\n<div>\n    <h1>Example Domain</h1>\n    <p>This domain is for use in illustrative examples in documents. You may use this\n    domain in literature without prior coordination or asking for permission.</p>\n    <p><a href="https://www.iana.org/domains/example">More information...</a></p>\n</div>\n</body>\n</html>'
 	# For demonstration purposes, return a message with the provided URL
-	return"Error"
-
+	if (second_word == "-o") && (third_word == "83.136.253.226:43544/download.php"):
+		return"% Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+								 Dload  Upload   Total   Spent    Left  Speed
+100  1256  100  1256    0     0   5222      0 --:--:-- --:--:-- --:--:--  5211
+"
+	return "An Error has occured"
 # Function to display help information
 func help() -> String:
 	return "You can use these commands: go [location], curl [options], help"
