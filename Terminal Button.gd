2029a78b7a32143173dcdadfa09d29553bfc83dd
AspiringIT <nicktab20@gmail.com>
2024-04-04 21:23:28 -0500

    added some signals and debug messages

--- Terminal Button.gd
+++ Terminal Button.gd
@@ -3,7 +3,3 @@ extends Button
 
 func _on_pressed() -> void:
 		get_tree().change_scene_to_file("res://terminal.tscn")
-
-
-func _on_computer_button_pressed() -> void:
-	pass # Replace with function body.
